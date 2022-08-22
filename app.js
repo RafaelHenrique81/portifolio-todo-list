43d54506d836df83fab46e3bab76994d84a9cae3
Rafael Henrique <rhsuptec@gmail.com>
2022-08-22 18:09:41 -0300

    implemented search function and changed code to manage trash functions

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const container = document.querySelector('.todos-container')
 form.addEventListener('submit',(event) => {
     event.preventDefault()
 
-    if(event.target.add.value.length){
+    if(event.target.add.value.trim().length){
         container.innerHTML += `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
+        <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${event.target.add.value.trim()}">
             <span>${event.target.add.value.trim()}</span>
-            <i class="far fa-trash-alt delete"></i>
+            <i class="far fa-trash-alt" data-trash="${event.target.add.value.trim()}"></i>
         </li>`
     }
     event.target.reset()
@@ -23,8 +23,26 @@ search.addEventListener('submit',(event) => {
 container.addEventListener('click', event => {
     const clickedElement = event.target
 
-    if(Array.from(clickedElement.classList).includes('delete')){
-        clickedElement.parentElement.remove()
+    if(clickedElement.dataset.trash) {
+        document.querySelector(`[data-todo="${clickedElement.dataset.trash}"]`).remove()
     }
 })
 
+search.addEventListener('input', event => {
+    const inputValue = event.target.value.trim().toLowerCase()
+
+    Array.from(container.children)
+        .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
+        .forEach(todo => {
+            todo.classList.remove('d-flex')
+            todo.classList.add('hidden')
+        })
+        
+    Array.from(container.children)
+        .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
+        .forEach(todo => {
+            todo.classList.remove('hidden')
+            todo.classList.add('d-flex')
+    })
+        
+})
